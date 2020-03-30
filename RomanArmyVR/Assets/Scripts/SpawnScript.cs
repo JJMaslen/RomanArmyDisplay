4cd1b2fad265a7631ad974e16c87ee6ad28adf6a
JJMaslen <44002981+JJMaslen@users.noreply.github.com>
2020-03-30 15:39:55 +0100

    Script to spawn them all is now implemented

---
+++ RomanArmyVR/Assets/Scripts/SpawnScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject trainingDummy;
+    int x;
+    int z;
+    int front;
+    int length;
+
+    void Start()
+    {
+        x = 0;
+        z = 0;
+        front = 0;
+        length = 120;
+        length = length * 10;
+
+        for (int i = 0; i < length; i++)
+        {
+            if (i % 120 == 0)
+            {
+                z = 0;
+                front += 50;
+            }
+
+            x += 5;
+
+            if (i % 8 == 0)
+            {
+                z += 5;
+                x = front;
+            }
+
+            GameObject tempObject = GameObject.Instantiate(trainingDummy);
+            tempObject.transform.position = new Vector3(x, 0, z);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
